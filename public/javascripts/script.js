864e0107d9b4aa73abb60b2340194ce25f980701
windtagane <windtagane@gmail.com>
2020-03-31 15:27:13 +0400

    affichage bootstrap

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,12 +7,13 @@ $(document).ready(function() {
 
     function showOneUser(idVal) {
         $.get(`/users/get/${idVal}`, function(user) {
-            
+
             $("#display-user-name").html(user.nom);
-            $("#display-user-age").html(user.age + " ans");
-            $("#display-user-job").html("Profession: " + user.profession);
-            $("#display-user-email").html("Email: " + user.email);
-            $("#display-user-tel").html("Téléphone: " + user.tel);
+            $("#display-user-age").html(user.age);
+            $("#display-user-job").html(user.profession);
+            $("#display-user-email").html(user.email);
+            $("#display-user-tel").html(user.tel);
+            $("#user-identity").show();
             
         })
     }
